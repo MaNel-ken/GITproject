b41ed1908ae12b8982d79b939ce2a4a02c735e9c
MaNel-ken <69055646+MaNel-ken@users.noreply.github.com>
2023-12-13 16:21:37 +0100

    Update and rename DemoApplication.java to SimpleJavaAppWithoutInput.java

---
+++ SimpleJavaAppWithoutInput.java
@@ -0,0 +1,6 @@
+public class SimpleJavaAppWithoutInput {
+    public static void main(String[] args) {
+        // Print a fixed message
+        System.out.println("Hello! This is a simple Java console application without user input.");
+    }
+}
